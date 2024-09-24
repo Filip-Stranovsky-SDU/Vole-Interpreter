f53008afdb6a8e024fabcb04816c11322a8a20f0
fistr24@student.sdu.dk <fistr24@student.sdu.dk>
2024-09-24 10:09:57 +0200

    Editing program.py and CPU.py UI for memory and register editing finished, added run button, basic run cycle

--- CPU.py
+++ CPU.py
@@ -1,9 +1,36 @@
 class CPU:
-    def __init__(self):
-        self.registers = [0] * 16
-        self.memory = [0]*256
-        self.pc = 0 #Program Counter
+
+    registers: list[int] = [0] * 16
+    memory: list[int] = [0]*256
+    pc: int = 0 #Program Counter
+    is_running: bool = False
+
+    def __init__(self, program):
+        self.program = program
 
     
     def parse_instruction(self, memory_adress: int):
         pass
+
+
+    def step(self):
+        instruction = self.parse_instruction(self.pc)
+        self.pc = self.pc + 2
+
+        match(instruction[0]):
+            case 1:
+                pass
+
+
+
+    def run(self):
+        self.step()
+        if self.is_running:
+            self.program.canvas.after(1000, self.run)
+    
+    def start_cpu_run(self):
+        if not self.is_running:
+            self.is_running = True
+            self.run()
+        else:
+            self.is_running = False
--- program.py
+++ program.py
@@ -1,38 +1,97 @@
 import tkinter as Tk
 import CPU
-
+from typing import List
 
 class App:
-    def __init__(self):
-        self.canvas = Tk.Canvas(width=800, height=600)
 
-        self.memory_entries = []
-        self.register_entries = []
+    root: Tk.Tk = Tk.Tk()
+    canvas: Tk.Canvas = Tk.Canvas(root, width=800, height=600)
+
+    memory_entries: list[Tk.Entry] = []
+    memory_entries_svars: list[Tk.StringVar] = []
+
+    register_entries: list[Tk.Entry] = []
+    register_entries_svars: list[Tk.StringVar] = []
+
+    run_button: Tk.Button
+
+    def __init__(self) -> None:
+        self.cpu: CPU = CPU.CPU(self)
 
         self.create_ui()
-        
-        self.cpu = CPU.CPU()
-
         self.canvas.mainloop()
 
-    def create_memory_entries(self):
-        for i in range(0, 16):
-            for j in range(0, 16):
-                new_e = Tk.Entry(width=2)
-                new_e.insert(0, f"{hex(j)[2:]}{hex(i)[2:]}")
-                new_e.place(x=i*20, y=j*20)
+    def create_memory_entries(self) -> None:
+        for j in range(0, 16):
+            for i in range(0, 16):
+                new_svar = Tk.StringVar(name=f"PY_SVAR_M{i + 16*j}", master=self.canvas)
+                new_svar.trace_add("write", self.check_entry_string_memory)
+
+                new_e = Tk.Entry(width=3, textvariable=new_svar)
+                new_e.place(x=i*25, y=j*20)
+
                 self.memory_entries.append(new_e)
+                self.memory_entries_svars.append(new_svar)
 
-    def create_register_entries(self):
+                new_svar.set(f"{hex(j)[2:]}{hex(i)[2:]}") #In the end because needs to append first so check can run
+
+    def create_register_entries(self) -> None:
         for i in range(0, 16):
-            new_e = Tk.Entry(width=2)
-            new_e.insert(0, f"r{hex(i)[2:]}")
+            new_svar = Tk.StringVar(name=f"PY_SVAR_R{i}", master=self.canvas)
+            new_svar.trace_add("write", self.check_entry_string_registers)
+            
+            new_e = Tk.Entry(width=2, textvariable=new_svar)
             new_e.place(x=400, y=i*20)
-            self.register_entries.append(new_e)
 
-    def create_ui(self):
+            self.register_entries.append(new_e)
+            self.register_entries_svars.append(new_svar)
+
+            new_svar.set(f"r{hex(i)[2:]}") #In the end because needs to append first so check can run
+
+
+
+    def create_ui(self) -> None:
         self.create_register_entries()
         self.create_memory_entries()
+
+        self.run_button = Tk.Button(text="run", command=self.cpu.start_cpu_run)
+        self.run_button.place(x=200, y=400)
+        
+
+
+    def check_entry_string_memory(self, var, index, mode) -> None:
+        address: int = int(var[9:])
+        svar: Tk.StringVar = self.memory_entries_svars[address]
+    
+        try: 
+            temp: int = int(svar.get(), 16)
+        except ValueError:
+            svar.set(hex(self.cpu.memory[address])[2:])
+            return
+        
+        if(temp > 255):
+            svar.set(hex(self.cpu.memory[address])[2:])
+            return
+        
+        self.cpu.memory[address] = temp
+        
+
+    def check_entry_string_registers(self, var, index, mode) -> None:
+        address: int = int(var[9:])
+        svar: Tk.StringVar = self.register_entries_svars[address]
+    
+        try: 
+            temp: int = int(svar.get(), 16)
+        except ValueError:
+            svar.set(hex(self.cpu.registers[address])[2:])
+            return
+        
+        if(temp > 255):
+            svar.set(hex(self.cpu.registers[address])[2:])
+            return
+        
+        self.cpu.registers[address] = temp
+
 def main():
     app = App()
 
