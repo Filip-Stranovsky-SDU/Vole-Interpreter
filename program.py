1878df141a4b6bef80dc320704f52ade882ea679
fistr24@student.sdu.dk <fistr24@student.sdu.dk>
2024-10-02 22:09:49 +0200

    UI in program redone again check works now, it is weird but works

--- program.py
+++ program.py
@@ -4,14 +4,13 @@ from typing import List
 
 class App:
 
-    root: Tk.Tk = Tk.Tk()
-    canvas: Tk.Canvas = Tk.Canvas(root, width=800, height=600)
+    #root: Tk.Tk = Tk.Tk()
+    canvas: Tk.Canvas = Tk.Canvas(width=800, height=600, name="canvas")
 
-    memory_entries: list[Tk.Entry] = []
-    memory_entries_svars: list[Tk.StringVar] = []
+    memory_texts: list[Tk.Entry] = []
+    register_texts: list[Tk.Entry] = []
 
-    register_entries: list[Tk.Entry] = []
-    register_entries_svars: list[Tk.StringVar] = []
+    last_text_changed: Tk.Text = None
 
     run_button: Tk.Button
     step_button: Tk.Button
@@ -24,40 +23,37 @@ class App:
         self.cpu: CPU = CPU.CPU(self)
 
         self.create_ui()
+        self.canvas.pack()
+        self.canvas.bind("<Button-1>", self.click)
         self.canvas.mainloop()
 
     def create_memory_entries(self) -> None:
         for j in range(0, 16):
             for i in range(0, 16):
-                new_svar = Tk.StringVar(name=f"PY_SVAR_M{i + 16*j}", master=self.canvas)
-                new_svar.trace_add("write", self.check_entry_string_memory)
-
-                new_e = Tk.Entry(width=3, textvariable=new_svar)
-                new_e.place(x=i*25, y=j*20)
-
-                self.memory_entries.append(new_e)
-                self.memory_entries_svars.append(new_svar)
-
-                new_svar.set(f"{hex(j)[2:]}{hex(i)[2:]}") #In the end because needs to append first so check can run
+                new_text = Tk.Text(self.canvas, width=2, height=1, name=f"py_text_m{i + 16*j}")
+                new_text.bind("<Button-1>", self.click)
+                new_text.place(x=10+i*25, y=10+j*20)
+                
+                self.memory_texts.append(new_text)
+                
+                new_text.insert("1.0", f"{hex(j)[2:]}{hex(i)[2:]}")
 
     def create_register_entries(self) -> None:
         for i in range(0, 16):
-            new_svar = Tk.StringVar(name=f"PY_SVAR_R{i}", master=self.canvas)
-            new_svar.trace_add("write", self.check_entry_string_registers)
+            new_text = Tk.Text(self.canvas, width=2, height=1, name=f"py_text_r{i}")
+            new_text.bind("<Button-1>", self.click)
             
-            new_e = Tk.Entry(width=2, textvariable=new_svar)
-            new_e.place(x=400, y=i*20)
+            new_text.place(x=450, y=10+i*20)
 
-            self.register_entries.append(new_e)
-            self.register_entries_svars.append(new_svar)
+            self.register_texts.append(new_text)
 
-            new_svar.set(f"r{hex(i)[2:]}") #In the end because needs to append first so check can run
-
+            new_text.insert("1.0", f"r{hex(i)[2:]}")
+            
 
 
     def create_ui(self) -> None:
         self.create_register_entries()
-        self.create_memory_entries()
+        self.create_memory_entries()        
 
         self.run_button = Tk.Button(text="run", command=self.cpu.start_cpu_run)
         self.run_button.place(x=200, y=400)
@@ -66,50 +62,50 @@ class App:
         
 
 
-    def check_entry_string_memory(self, var, index, mode) -> None:
-        address: int = int(var[9:])
-        svar: Tk.StringVar = self.memory_entries_svars[address]
+    def click(self, event: Tk.Event) -> None:
+        if self.last_text_changed != None and self.last_text_changed._name != "canvas":
+            self.check_last_change()
+        self.last_text_changed = event.widget
 
-        print(svar.get())
-        return
-
-        try: 
-            temp: int = int(svar.get(), 16)
+    def check_last_change(self) -> None:
+        name: str = self.last_text_changed._name
+        value: str = self.last_text_changed.get("1.0", Tk.END)
+        value = value.strip()[-2:]
+        print(value)
+        try:
+            value_i = int(value, 16)
+            if value_i > 255:
+                raise ValueError
         except ValueError:
-            svar.set(hex(self.cpu.memory[address])[2:])
+            self.last_text_changed.delete("1.0", Tk.END)
+            if name[8] == "m":
+                val =  self.cpu.memory[int(name[9:])]
+            if name[8] == "r":
+                val = self.cpu.registers[int(name[9:])]
+            print(val)
+            self.last_text_changed.insert("1.0", hex(val)[2:])
             return
+        if len(value) == 1:
+            self.last_text_changed.insert("1.0", "0")
+
+        if name[8] == "m":
+            self.cpu.memory[int(name[9:])] = value_i
+        if name[8] == "r":
+            self.cpu.registers[int(name[9:])] = value_i
         
-        if(temp > 255):
-            svar.set(hex(self.cpu.memory[address])[2:])
-            return
-        
-        self.cpu.memory[address] = temp
         
 
-    def check_entry_string_registers(self, var, index, mode) -> None:
-        address: int = int(var[9:])
-        svar: Tk.StringVar = self.register_entries_svars[address]
-    
-        try: 
-            temp: int = int(svar.get(), 16)
-        except ValueError:
-            svar.set(hex(self.cpu.registers[address])[2:])
-            return
-        
-        if(temp > 255):
-            svar.set(hex(self.cpu.registers[address])[2:])
-            return
-        
-        self.cpu.registers[address] = temp
 
 
     def change_memory_ui_val(self, address):
         new_val = hex(self.cpu.memory[address])[2:]
-        self.memory_entries_svars[address].set(new_val)
+        self.memory_texts[address].delete("1.0", Tk.END)
+        self.memory_texts[address].insert("1.0", new_val)
 
     def change_register_ui_val(self, address):
-        new_val = hex(self.cpu.registers[address])[2:]
-        self.register_entries_svars[address].set(new_val)
+        new_val = hex(self.cpu.memory[address])[2:]
+        self.register_texts[address].delete("1.0", Tk.END)
+        self.register_texts[address].insert("1.0", new_val)
 
 def main():
     app = App()
